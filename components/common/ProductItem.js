7fa419a679de8341b624e131d2ae81574f4fda15
chenyucai <e_ycchen@126.com>
2017-03-23 15:41:35 +0800

    finish home_page

---
+++ components/common/ProductItem.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image
+} from 'react-native'
+
+export default class ProductItemComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // props 需要传入图片的宽度和高度
+  render () {
+    return (
+      <View style={[styles.item, {width:this.props.width}]}>
+        <View style={{
+          width: this.props.width,
+          height: this.props.height
+        }}>
+          <Image style={{
+            width: this.props.width,
+            height: this.props.height
+          }} source={require('../home/image/AF89940B-AB2E-480F-ABF4-F7FE9B3A6C0D.png')}/>
+        </View>
+
+        <View style={styles.item_content}>
+          <View style={styles.item_title}>
+            <Text style={styles.item_title_text} numberOfLines={2}>
+              <Text style={{color:'#363334'}}>【紧肤系列】</Text>
+              <Text style={{color:'#292929'}}>润白颜水光针2ml+伊肤泉无菌修复美颜</Text>
+            </Text>
+          </View>
+          <View style={styles.item_footer}>
+            <View style={styles.item_price}>
+              <Text style={styles.item_price_now}>￥1000</Text>
+              <Text style={styles.item_price_old}>¥1500</Text>
+            </View>
+            <View style={styles.item_sale}>
+              <Text style={{fontSize:10}}>已售</Text>
+              <Text style={styles.item_count}>2000</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.item_collection}>
+          <Image source={require('./image/icon_dis_love.png')} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  item:{
+    backgroundColor:'#fff'
+  },
+  item_title:{
+    alignSelf:'center',
+    marginTop:7
+  },
+  item_title_text:{
+    fontSize: 11,
+    lineHeight: 16
+  },
+  item_footer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft:10,
+    paddingRight:10,
+    marginBottom: 8,
+    marginTop: 21
+  },
+  item_sale:{
+    flexDirection: 'row',
+    alignItems:'center'
+  },
+  item_price:{
+    flexDirection: 'row',
+    alignItems:'center'
+  },
+  item_price_now:{
+    fontSize:14,
+    color:'#FF5D94',
+    marginRight:3
+  },
+  item_price_old:{
+    fontSize:10,
+    color:'#C9C6C6'
+  },
+  item_count:{
+    fontSize:10
+  },
+  item_collection:{
+    position: 'absolute',
+    right: 10,
+    top: 10
+  }
+});
--- components/home/Home.js
+++ components/home/Home.js
@@ -15,6 +15,7 @@ import HomeHeaderComponent from './HomeHeader';
 import BottomTabsComponent from '../common/BottomTabs';
 import HomeModulesComponent from './HomeModules';
 import HomeBlockTitleComponent from './HomeBlockTitle';
+import ProductItemComponent from '../common/ProductItem';
 
 import ScreenUtils from '../../utils/ScreenUtils';
 
@@ -65,8 +66,42 @@ export default class HomeComponent extends Component {
     return saleItems;
   }
 
+  renderMagazine() {
+    let magazineItems = [];
+    for (var i = 0; i < 6; i++) {
+      magazineItems.push(
+        <Image source={require('./image/BFABCD35-8EEF-49CB-B5DE-E3042232E816.png')}
+          key={i}
+          style={styles.magazine_item}
+        />
+      )
+    }
+    return magazineItems;
+  }
+
+  renderBeautiful() {
+    let items = [];
+    for (var i = 0; i < 3; i++) {
+      items.push(
+        <Image source={require('./image/8832F62B-2070-45C5-96E3-2BCB4F5CBE3F.png')}
+          key={i}
+          style={{
+            width: ScreenUtils.scaleSize(375),
+            height: ScreenUtils.scaleSize(230),
+            marginBottom: 10
+          }}
+        />
+      )
+    }
+    return items;
+  }
+
   render () {
     const swiperHeight = Dimensions.get('window').width / 1.875;
+    let ProductItemInfo = {
+      width: 170,
+      height:150
+    }
     return (
       <View style={styles.wrapper}>
         <HomeHeaderComponent />
@@ -99,7 +134,75 @@ export default class HomeComponent extends Component {
             </ScrollView>
           </View>
 
-          {/*  */}
+          {/* 杂志社 */}
+          <View style={styles.magazine_wrapper}>
+            <View style={styles.block_title_wapper}>
+              <HomeBlockTitleComponent titleEn="Magazine Agency" titleZh="杂志社"/>
+            </View>
+            <View style={styles.magazine_body}>
+              <View style={styles.magazine_top}>
+                <Image source={require('./image/C29105FA-A819-40D0-831E-C7AB324AB618.png')}
+                  style={{width:ScreenUtils.scaleSize(375), height:ScreenUtils.scaleSize(230)}}
+                />
+              </View>
+              <ScrollView horizontal style={styles.magazine_list}>
+                {this.renderMagazine()}
+              </ScrollView>
+            </View>
+          </View>
+
+          {/* 美丽互生 */}
+          <View style={styles.beautiful_wrapper}>
+            <View style={styles.block_title_wapper}>
+              <HomeBlockTitleComponent titleEn="Beautiful alternate" titleZh="美丽互生"/>
+            </View>
+            <View style={styles.beautiful_body}>
+              {this.renderBeautiful()}
+            </View>
+          </View>
+
+          {/* 推荐商家 */}
+          <View style={styles.store_wrapper}>
+            <View style={styles.block_title_wapper}>
+              <HomeBlockTitleComponent titleEn="Recommended businesses" titleZh="推荐商家"/>
+            </View>
+            <View style={styles.store_body}>
+              {/* {this.renderStore()} */}
+              <View style={styles.store_item}>
+                <Image style={styles.store_img} source={require('./image/46037860-08A1-4A1B-89BB-9CCB03C63BD5.png')}/>
+              </View>
+              <View style={styles.store_item}>
+                <Image style={styles.store_img} source={require('./image/46037860-08A1-4A1B-89BB-9CCB03C63BD5.png')}/>
+              </View>
+              <View style={styles.store_item}>
+                <Image style={styles.store_img} source={require('./image/46037860-08A1-4A1B-89BB-9CCB03C63BD5.png')}/>
+              </View>
+              <View style={styles.store_item}>
+                <Image style={styles.store_img} source={require('./image/46037860-08A1-4A1B-89BB-9CCB03C63BD5.png')}/>
+              </View>
+            </View>
+          </View>
+
+          {/* 全面大热购 */}
+          <View style={styles.hot_wrapper}>
+            <View style={[styles.block_title_wapper,{backgroundColor:'#F0EFF5'}]}>
+              <HomeBlockTitleComponent titleEn="All the big Tesoo" titleZh="全面大热购"/>
+            </View>
+            <View style={styles.hot_body}>
+              <View style={styles.hot_item}>
+                <ProductItemComponent {...ProductItemInfo}/>
+              </View>
+              <View style={styles.hot_item}>
+                <ProductItemComponent {...ProductItemInfo}/>
+              </View>
+              <View style={styles.hot_item}>
+                <ProductItemComponent {...ProductItemInfo}/>
+              </View>
+              <View style={styles.hot_item}>
+                <ProductItemComponent {...ProductItemInfo}/>
+              </View>
+            </View>
+          </View>
         </ScrollView>
 
         <View style={styles.tabs}>
@@ -129,6 +232,7 @@ const styles = StyleSheet.create({
   block_title_wapper:{
     paddingTop: 20,
     paddingBottom:20,
+    backgroundColor:'#fff'
   },
   sale_body: {
     height: 230,
@@ -168,5 +272,55 @@ const styles = StyleSheet.create({
   sale_price_old:{
     fontSize:10,
     color:'#C9C6C6'
-  }
+  },
+  magazine_wrapper:{
+    marginTop:10
+  },
+  magazine_body:{
+    backgroundColor:'#F0EFF5'
+  },
+  magazine_List: {
+    backgroundColor:'#fff',
+    flexDirection:'row',
+  },
+  magazine_item:{
+    width:118,
+    height:135,
+    marginRight: 12,
+    marginTop:10,
+    marginBottom:20
+  },
+  store_body:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'space-around',
+    alignItems:'center',
+    paddingLeft:5,
+    paddingRight:5,
+    paddingTop:5,
+    paddingBottom:5,
+    backgroundColor:'#fff'
+  },
+  store_item:{
+    width: ScreenUtils.scaleSize(170),
+    height: ScreenUtils.scaleSize(170),
+    marginBottom:10
+  },
+  store_img:{
+    width: ScreenUtils.scaleSize(170),
+    height: ScreenUtils.scaleSize(170)
+  },
+  hot_body:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'space-around',
+    alignItems:'center',
+    paddingLeft:5,
+    paddingRight:5,
+    paddingTop:5,
+    paddingBottom:5,
+  },
+  hot_item:{
+    marginBottom:10
+  },
 });
