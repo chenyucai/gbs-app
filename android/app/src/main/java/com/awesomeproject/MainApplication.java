6015e6ca8f8c959a30fa2e4d6d63b3c15b5dcccc
chenyucai <e_ycchen@126.com>
2017-04-10 17:38:49 +0800

    ready fix

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.beefe.picker.PickerViewPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -29,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new PickerViewPackage(),
-            new MapsPackage()
+            new PickerViewPackage()
       );
     }
   };
--- src/Action/PersonalCenter/PersonalInfo/PersonalInfo.js
+++ src/Action/PersonalCenter/PersonalInfo/PersonalInfo.js
@@ -9,49 +9,280 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
+  Dimensions,
+  Animated,
+  Easing,
   // Picker
 } from 'react-native'
 import Picker from 'react-native-picker';
 
 import BaseNavigationBar from '../../../BaseView/BaseNavigationBar/BaseNavigationBar';
-import MyPicker from '../../../BaseView/Picker/MyPicker';
+import area from './area.json';
 
 export default class PersonalInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hide: true,
-      opt:'1'
+      opacity: new Animated.Value(0),
+      maskShow: false,
+      sex:'',
+      birth:'',
+      area:'',
+      job:'',
+      pos:'',
+      income:''
     };
   }
-  componentDidMount(){
 
+  _createDateData() {
+      let date = [];
+      for(let i=1950;i<2050;i++){
+          let month = [];
+          for(let j = 1;j<13;j++){
+              let day = [];
+              if(j === 2){
+                  for(let k=1;k<29;k++){
+                      day.push(k+'日');
+                  }
+                  //Leap day for years that are divisible by 4, such as 2000, 2004
+                  if(i%4 === 0){
+                      day.push(29+'日');
+                  }
+              }
+              else if(j in {1:1, 3:1, 5:1, 7:1, 8:1, 10:1, 12:1}){
+                  for(let k=1;k<32;k++){
+                      day.push(k+'日');
+                  }
+              }
+              else{
+                  for(let k=1;k<31;k++){
+                      day.push(k+'日');
+                  }
+              }
+              let _month = {};
+              _month[j+'月'] = day;
+              month.push(_month);
+          }
+          let _date = {};
+          _date[i+'年'] = month;
+          date.push(_date);
+      }
+      return date;
+  }
 
+  _createAreaData() {
+      let data = [];
+      let len = area.length;
+      for(let i=0;i<len;i++){
+          let city = [];
+          for(let j=0,cityLen=area[i]['city'].length;j<cityLen;j++){
+              let _city = {};
+              _city[area[i]['city'][j]['name']] = area[i]['city'][j]['area'];
+              city.push(_city);
+          }
+
+          let _data = {};
+          _data[area[i]['name']] = city;
+          data.push(_data);
+      }
+      return data;
+  }
+
+  _showDatePicker() {
+    this.setState({maskShow:true},this.in);
+    Picker.init({
+        pickerData: this._createDateData(),
+        pickerConfirmBtnText:'确定',
+        pickerCancelBtnText:'取消',
+        pickerTitleText:'',
+        pickerConfirmBtnColor:[255,109,153,1],
+        pickerCancelBtnColor:[184,184,184,1],
+        pickerBg:[240,239,245,1],
+        onPickerConfirm: (pickedValue, pickedIndex) => {
+            console.log('date', pickedValue, pickedIndex);
+            this.out();
+            this.setState({birth: pickedValue});
+        },
+        onPickerCancel: (pickedValue, pickedIndex) => {
+            console.log('date', pickedValue, pickedIndex);
+            this.out()
+        },
+        onPickerSelect: (pickedValue, pickedIndex) => {
+            console.log('date', pickedValue, pickedIndex);
+        }
+    });
+    Picker.show();
+  }
+
+  _showAreaPicker() {
+    this.setState({maskShow:true},this.in);
+    Picker.init({
+        pickerData: this._createAreaData(),
+        selectedValue: ['河北', '唐山', '古冶区'],
+        pickerConfirmBtnText:'确定',
+        pickerCancelBtnText:'取消',
+        pickerTitleText:'',
+        pickerConfirmBtnColor:[255,109,153,1],
+        pickerCancelBtnColor:[184,184,184,1],
+        pickerBg:[240,239,245,1],
+        onPickerConfirm: pickedValue => {
+            console.log('area', pickedValue);
+            this.out();
+            this.setState({area: pickedValue});
+        },
+        onPickerCancel: pickedValue => {
+            console.log('area', pickedValue);
+            this.out()
+        },
+        onPickerSelect: pickedValue => {
+            //Picker.select(['山东', '青岛', '黄岛区'])
+            console.log('area', pickedValue);
+        }
+    });
+    Picker.show();
+  }
+
+  _showSexPicker() {
+    this.setState({maskShow:true},this.in);
+    let data = [
+      '男',
+      '女',
+    ];
+    Picker.init({
+      pickerData: data,
+      selectedValue: ['女'],
+      pickerConfirmBtnText:'确定',
+      pickerCancelBtnText:'取消',
+      pickerTitleText:'',
+      pickerConfirmBtnColor:[255,109,153,1],
+      pickerCancelBtnColor:[184,184,184,1],
+      pickerBg:[240,239,245,1],
+      onPickerConfirm: data => {
+          console.log(data);
+          this.out();
+          this.setState({sex: data});
+      },
+      onPickerCancel: data => {
+          console.log(data);
+          this.out();
+      },
+      onPickerSelect: data => {
+          console.log(data);
+      }
+    });
+    Picker.show();
+  }
+
+  _showJobPicker() {
+    this.setState({maskShow:true},this.in);
+    let data = [
+      '自由职业装',
+      '普通员工',
+      '部门经理',
+      '公司总监'
+    ];
+    Picker.init({
+      pickerData: data,
+      selectedValue: ['部门经理'],
+      pickerConfirmBtnText:'确定',
+      pickerCancelBtnText:'取消',
+      pickerTitleText:'职位',
+      pickerConfirmBtnColor:[255,109,153,1],
+      pickerCancelBtnColor:[184,184,184,1],
+      pickerBg:[240,239,245,1],
+      onPickerConfirm: data => {
+          console.log(data);
+          this.out()
+          this.setState({job: data});
+      },
+      onPickerCancel: data => {
+          console.log(data);
+          this.out();
+      },
+      onPickerSelect: data => {
+          console.log(data);
+      }
+    });
+    Picker.show();
+  }
+
+  _showPosPicker() {
+    this.setState({maskShow:true},this.in);
+    let data = [
+      '自由职业装',
+      '普通员工',
+      '部门经理',
+      '公司总监'
+    ];
+    Picker.init({
+      pickerData: data,
+      selectedValue: ['部门经理'],
+      pickerConfirmBtnText:'确定',
+      pickerCancelBtnText:'取消',
+      pickerTitleText:'职位',
+      pickerConfirmBtnColor:[255,109,153,1],
+      pickerCancelBtnColor:[184,184,184,1],
+      pickerBg:[240,239,245,1],
+      onPickerConfirm: data => {
+          console.log(data);
+          this.out();
+          this.setState({pos: data});
+      },
+      onPickerCancel: data => {
+          console.log(data);
+          this.out();
+      },
+      onPickerSelect: data => {
+          console.log(data);
+      }
+    });
+    Picker.show();
+  }
+
+  _showIncomePicker() {
+    this.setState({maskShow:true},this.in);
+    let data = [
+      '0-10万',
+      '10-15万',
+      '15－25万',
+      '25-50万'
+    ];
+    Picker.init({
+      pickerData: data,
+      selectedValue: ['0-10万'],
+      pickerConfirmBtnText:'确定',
+      pickerCancelBtnText:'取消',
+      pickerTitleText:'',
+      pickerConfirmBtnColor:[255,109,153,1],
+      pickerCancelBtnColor:[184,184,184,1],
+      pickerBg:[240,239,245,1],
+      onPickerConfirm: data => {
+          console.log(data);
+          this.out();
+          this.setState({income: data});
+      },
+      onPickerCancel: data => {
+          console.log(data);
+          this.out();
+      },
+      onPickerSelect: data => {
+          console.log(data);
+      }
+    });
+    Picker.show();
   }
 
   render () {
-    let data = [];
-    for(var i=0;i<100;i++){
-        data.push(i);
-    }
-
-    Picker.init({
-        pickerData: data,
-        selectedValue: [59],
-        onPickerConfirm: data => {
-            console.log(data);
-            this.setState({opt:data})
-        },
-        onPickerCancel: data => {
-            console.log(data);
-        },
-        onPickerSelect: data => {
-            console.log(data);
-        }
-    });
-    Picker.hide();
     return (
       <View style={styles.container}>
+        {
+          this.state.maskShow
+            ?
+              <Animated.View style={ styles.mask } >
+              </Animated.View>
+            : null
+        }
+
         <BaseNavigationBar
             data={{
                 title: "资料详情",
@@ -128,7 +359,7 @@ export default class PersonalInfo extends Component {
                 邀请码是为您唯一定制的身份标识，把邀请码分享给您的朋友前来注册后，您可以丰厚获奖励
               </Text>
             </View>
-            <TouchableOpacity style={[styles.item]} onPress={() => {Picker.show()}}>
+            <TouchableOpacity style={[styles.item]} onPress={this._showJobPicker.bind(this)}>
               <Text style={styles.item_label}>意向服务</Text>
               <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
                 <Text style={{fontSize:12, color:"#B8B8B8"}}>鼻部  面部整形  美甲</Text>
@@ -137,9 +368,51 @@ export default class PersonalInfo extends Component {
             </TouchableOpacity>
           </View>
 
-          <Text>{this.state.opt}</Text>
+          <View style={styles.block}>
+            <TouchableOpacity style={[styles.item]} onPress={this._showSexPicker.bind(this)}>
+              <Text style={styles.item_label}>性别</Text>
+              <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
+                <Text style={{fontSize:12}}>{this.state.sex!=''?this.state.sex:'请选择'}</Text>
+              </View>
+              <Image source={require('./assets/icon_Get into3.png')} style={{}}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.item]} onPress={this._showDatePicker.bind(this)}>
+              <Text style={styles.item_label}>出生日期</Text>
+              <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
+                <Text style={{fontSize:12}}>{this.state.birth!=''?this.state.birth:'请选择'}</Text>
+              </View>
+              <Image source={require('./assets/icon_Get into3.png')} style={{}}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.item]} onPress={this._showAreaPicker.bind(this)}>
+              <Text style={styles.item_label}>常住地</Text>
+              <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
+                <Text style={{fontSize:12}}>{this.state.area!=''?this.state.area:'请选择'}</Text>
+              </View>
+              <Image source={require('./assets/icon_Get into3.png')} style={{}}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.item]} onPress={this._showJobPicker.bind(this)}>
+              <Text style={styles.item_label}>行业</Text>
+              <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
+                <Text style={{fontSize:12}}>{this.state.job!=''?this.state.job:'请选择'}</Text>
+              </View>
+              <Image source={require('./assets/icon_Get into3.png')} style={{}}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.item]} onPress={this._showPosPicker.bind(this)}>
+              <Text style={styles.item_label}>当前职位</Text>
+              <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
+                <Text style={{fontSize:12}}>{this.state.pos!=''?this.state.pos:'请选择'}</Text>
+              </View>
+              <Image source={require('./assets/icon_Get into3.png')} style={{}}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.item]} onPress={this._showIncomePicker.bind(this)}>
+              <Text style={styles.item_label}>家庭年收入</Text>
+              <View style={{flex:1,alignItems:'flex-end',paddingRight:8}}>
+                <Text style={{fontSize:12}}>{this.state.income!=''?this.state.income:'请选择'}</Text>
+              </View>
+              <Image source={require('./assets/icon_Get into3.png')} style={{}}/>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
-        <MyPicker options={['1','2','3']} hide={this.state.hide} callback={this.change.bind(this)}/>
       </View>
     )
   }
@@ -149,8 +422,41 @@ export default class PersonalInfo extends Component {
       opt:opt
     })
   }
+
+  //显示动画
+  in() {
+    Animated.parallel([
+      Animated.timing(
+        this.state.opacity,
+        {
+          easing: Easing.linear,
+          duration: 300,
+          toValue: 0.8,
+        }
+      )
+    ]).start();
+  }
+  //隐藏动画
+  out(){
+    Animated.parallel([
+      Animated.timing(
+        this.state.opacity,
+        {
+          easing: Easing.linear,
+          duration: 300,
+          toValue: 0,
+        }
+      )
+    ]).start();
+    this.timer = setTimeout(
+      () => this.setState({maskShow: false}),
+      500
+    );
+  }
 }
 
+const {width, height} = Dimensions.get('window');
+const [left, top] = [0, 0];
 const styles = StyleSheet.create({
   container:{
     flex:1,
@@ -172,5 +478,20 @@ const styles = StyleSheet.create({
   border_bottom:{
     borderBottomWidth:0.5,
     borderBottomColor:"#d7d7d7"
-  }
+  },
+  block:{
+    backgroundColor:'#fff',
+    marginBottom:10
+  },
+  mask: {
+    justifyContent:"center",
+    backgroundColor:"#383838",
+    opacity:0.8,
+    position:"absolute",
+    width:width,
+    height:height,
+    left:left,
+    top:top,
+    zIndex:100
+  },
 });
